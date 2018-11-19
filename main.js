be8c22ccbbb692e472b410eacbac15aca9df0954
Qasmi2 <nadeemqasmi40@yahoo.com>
2018-11-19 15:37:09 +0500

    updated media section

---
+++ main.js
@@ -0,0 +1,30 @@
+
+window.onload = function displayBlog() {
+    document.getElementById("blog").innerHTML = "Irtif Group at Zameen Expo Perfomance Award Ceremory Ground Breaking Cermony ";
+    document.getElementById("gallery").style.display = "none";
+    document.getElementById("news").style.display = "none";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("blog").style.borderLeft = "thick solid";
+    window.onload();
+}
+function displayBlog() {
+    document.getElementById("blog").innerHTML = "Irtif Group at Zameen Expo Perfomance Award Ceremory Ground Breaking Cermony";
+    document.getElementById("gallery").style.display = "none";
+    document.getElementById("news").style.display = "none";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("blog").style.borderLeft = "thick solid";
+}
+function displayGallery() {
+    document.getElementById("gallery").innerHTML = "Irtif Group at Zameen Expo Perfomance Award Ceremory Ground Breaking Cermony";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("news").style.display = "none";
+    document.getElementById("gallery").style.display = "block";
+    document.getElementById("gallery").style.borderLeft = "thick solid";
+}
+function displayNews() {
+    document.getElementById("news").innerHTML = "Irtif Group at Zameen Expo Perfomance Award Ceremory Ground Breaking Cermony";
+    document.getElementById("gallery").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("news").style.display = "block";
+    document.getElementById("news").style.borderLeft = "thick solid";
+}
